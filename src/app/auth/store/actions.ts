a2f36be7c336cc6138ae3d6cc0ab331d08fc4d29
mouloud78 <mouloudouldali78@gmail.com>
2024-10-15 13:34:43 -0400

    Registration

---
+++ src/app/auth/store/actions.ts
@@ -0,0 +1,13 @@
+import {createActionGroup, props} from '@ngrx/store'
+import {BackendErrorsInterface} from 'src/app/shared/types/backendErrors.interface'
+import {CurrentUserInterface} from 'src/app/shared/types/currentUser.interface'
+import {RegisterRequestInterface} from '../types/registerRequest.interface'
+
+export const authActions = createActionGroup({
+  source: 'auth',
+  events: {
+    Register: props<{request: RegisterRequestInterface}>(),
+    'Register success': props<{currentUser: CurrentUserInterface}>(),
+    'Register failure': props<{errors: BackendErrorsInterface}>(),
+  },
+})
